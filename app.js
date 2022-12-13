ac63675ab79e48ae869a40d7b5f8a2876e5b1838
scRaul <61263078+scRaul@users.noreply.github.com>
2022-12-12 16:44:56 -0800

    Updated Look

--- app.js
+++ app.js
@@ -1,33 +1,149 @@
-var timer = new Timer(); 
-timer.setToCountUp(); 
+var barCountInput = document.getElementById('barCount');
+barCountInput.addEventListener('change',adjustBarCount);
 
-var list = []; 
-var copy = [];
-for(let i = 0; i < 100; i++){
-    list[i] = i;
-    copy[i] = i;
-}
-randomizeList(list);
-swapList = []
-quickSort(list,0,list.length-1);
 
-var index = -1; 
-console.log(copy);
-SortNext();
-function SortNext(){
-    if(swapList.length <= 0 ) return; 
-    if(index >= swapList.length){
-        console.log(copy);
-        return;
-    } 
-    index++;
-    var a = swapList[index][0];
-    var b = swapList[index][1];
-    AnimateSwap(a,b);
+const incrementsX = 500;
+const yDiff = 2; 
+
+const debugging = true; 
+
+var screenW = 501; 
+var screenH = screenW / yDiff; 
+const incrementsY = incrementsX / yDiff;
+
+var screen = new Screen(screenW,screenH,incrementsX,'screenA');
+
+var input = new Input();
+var sorter = new BarSorter(); 
+
+const fastFPS = 120; 
+const slowFPS = 10; 
+var fps = slowFPS; 
+
+
+
+var barList = [];
+var hList = []; // going to use to sort
+
+function generateBars(bars=10){
+    barList = [];
+    hList = [];
+    var barW = (screenW) / bars; 
+    var startX = -incrementsX/2 + barW/2;
+    for(let i = 1; i <= bars; i++){
+        var barH = (i / bars) * screenH;
+        var pos = new Vec(startX + (i-1)*barW,(-incrementsY/2) + barH/2  );
+        var color = new Color(0,255-bars+i,255-bars+i); 
+        var bar = new Bar(barW,barH,pos, color);
+        barList.push(bar);
+        hList.push(i);
+    }
 }
-async function AnimateSwap(a,b){
-    console.log("swapping"+a+','+b);
-    swap(copy,a,b);
-    await new Promise(r => setTimeout(r,50));
-    SortNext();
+
+adjustBarCount();
+
+
+var dynamic = [];
+var static = [];
+var animiating = false;
+var active  = 0; 
+loop();
+async function loop(){
+    var startTime = Date.now();
+    var delay = 1000/ fps; 
+    screen.clearColor();
+    handleInput(); 
+    updateList();
+    screen.draw(static,DRAW_MODE.SCREENOBJ);
+    screen.draw(dynamic,DRAW_MODE.SCREENOBJ);
+    screen.render();
+    await new Promise(r => setTimeout(r,(startTime+delay)-Date.now() ));
+    loop(); 
 }
+function handleInput(){
+    if(!debugging) return;
+    if(input.keys.one){
+        input.keys.one = false;
+        if(animiating) return;
+        sorter.randomizeBars(hList,barList);
+        animiating  = true; 
+    }
+    if(input.keys.two){
+        input.keys.two = false;
+        if(animiating) return;
+        sorter.sort(hList,barList,SORT_TYPE.QUICK);
+    }
+    if(input.keys.three){
+        input.keys.two = false;
+        if(animiating) return;
+        sorter.sort(hList,barList,SORT_TYPE.INSERT);
+    }
+    if(input.keys.four){
+        input.keys.two = false;
+        if(animiating) return;
+        sorter.sort(hList,barList,SORT_TYPE.BUBBLE);
+    }
+}
+function updateList(){
+    static = [];
+    dynamic = [];
+    active = 0;
+    for(let i = 0; i < barList.length;i++){ 
+        barList[i].update();
+        if(barList[i].isMoving()==true){
+            active++;
+            dynamic.push(barList[i].getObj());
+        }else{
+            static.push(barList[i].getObj());
+        }
+    }
+    if(active > 0){
+        fps = fastFPS;
+    }else{
+        fps = slowFPS;
+        animiating = false;
+    }
+   
+}
+function adjustBarCount(){
+    animiating =false;
+    let n = parseInt(barCountInput.value);
+    if(n < 5) return; 
+    if(n > 254) 
+    n = 255; 
+    generateBars(n);
+}
+function makeRandom(){
+    if(animiating) return;
+    adjustBarCount();
+    sorter.randomizeBars(hList,barList);
+    animiating = true;
+    document.getElementById('quickSort').style.display = 'none';
+    document.getElementById('insertSort').style.display = 'none';
+    document.getElementById('bubbleSort').style.display = 'none';
+}
+function Quick(){
+    if(animiating) return;
+    sorter.sort(hList,barList,SORT_TYPE.QUICK);
+    animiating = true;
+    document.getElementById('quickSort').style.display = 'block';
+    document.getElementById('insertSort').style.display = 'none';
+    document.getElementById('bubbleSort').style.display = 'none';
+}
+function Insert(){
+    if(animiating) return;
+    sorter.sort(hList,barList,SORT_TYPE.INSERT);
+    animiating = true;
+    document.getElementById('quickSort').style.display = 'none';
+    document.getElementById('insertSort').style.display = 'block';
+    document.getElementById('bubbleSort').style.display = 'none';
+}
+function Bubble(){
+    if(animiating) return;
+    sorter.sort(hList,barList,SORT_TYPE.BUBBLE);
+    animiating = true;
+    document.getElementById('quickSort').style.display = 'none';
+    document.getElementById('insertSort').style.display = 'none';
+    document.getElementById('bubbleSort').style.display = 'block';
+}
+
---
+++ src/bar.js
@@ -0,0 +1,151 @@
+class Bar{
+    #obj; // holds screen obj 
+    #posX;  // pos where its going to be // might not be current
+    #targetQ; // list of targeted pos
+    #targetX; // current target 
+    #bMoving;  // keep track if its moving 
+    #ogColor;
+    #w;
+    #h;
+    constructor(w,h,pos,color){
+        this.#w = w;
+        this.#h = h;
+        this.#obj = new ScreenObj(w,h,color); 
+        this.#obj.teleport(pos); 
+        this.#posX = pos.x;
+        this.#targetQ = [];
+        this.#bMoving = false;
+        this.#targetX = null;
+        this.#ogColor = color;
+    }
+    setColor(c){this.#obj.setColor(c);}
+    getCopy(){
+        return new Bar(this.#w,this.#h,this.#obj.getPos(),this.#ogColor);
+    }
+    getObj(){return this.#obj;}
+    addTarget(x){
+        this.#posX = x;
+        this.#targetQ.push(x); 
+    }
+    isMoving(){return this.#bMoving;}
+    getPos(){return this.#posX; }
+    update(){
+        this.#bMoving = true;
+        //if current target is null either no target left, or one not assigned yet
+        if(this.#targetX == null ){
+            if(this.#targetQ.length == 0 ){
+                this.#bMoving = false; // not target return 
+                return; 
+            }else{
+                this.#targetX = this.#targetQ.shift(); //get first target
+            }
+        }
+        // get distance from target location 
+        var dist = this.#targetX -  this.#obj.getPos().x; 
+        // if within 1, teleport to it 
+        if( Math.abs(dist) < 1 ){
+            this.#obj.teleport(new Vec(this.#targetX,this.#obj.getPos().y));
+            this.#bMoving = false; 
+            this.#targetX = null;
+        }else{ // move half the distance 
+            this.#obj.translate(dist/2,0);
+        }
+    }
+}
+const SORT_TYPE ={NONE:0,QUICK:1,INSERT:2,BUBBLE: 3}
+class BarSorter{
+    //pass in height list + bar list, specify sorting type opt QUICK, INSERT, BUBBLE
+    async sort(hList,barList,sortType){
+        switch(sortType){
+            case SORT_TYPE.QUICK:
+                this.quickSortBars(hList,barList,0,hList.length-1)
+                break;
+            case SORT_TYPE.INSERT:
+                this.insertSortBars(hList,barList);
+                break;
+            case SORT_TYPE.BUBBLE:
+                this.bubbleSortBars(hList,barList);
+                break;
+        }
+    }
+    //takes in height list along with  a list of bar List ( in order to randomize the same )
+    randomizeBars(hList,barList){
+        var repeat =hList.length*2; 
+        for(let i = 0; i < repeat;i++){
+            let a = Math.floor(Math.random() * hList.length); 
+            let b = Math.floor(Math.random() * hList.length);     
+            this.swap(hList,a,b);
+            this.swapBars(barList,a,b);
+        }    
+    }
+    swap(list,a,b){
+        var t = list[a];
+        list[a] = list[b];
+        list[b] = t; 
+    }
+    //swap bar a with b 
+    swapBars(barList,a,b){
+        let posA = barList[a].getPos();
+        barList[a].addTarget(barList[b].getPos());
+        barList[b].addTarget(posA);
+        this.swap(barList,a,b);
+    }
+    async bubbleSortBars(hList,barList){
+        for(let i = 0; i < hList.length-1;i++){
+            for(let j = 0; j < hList.length-i-1;j++){
+                if(hList[j] > hList[j+1]){
+                    this.swap(hList,j,j+1);
+                    this.swapBars(barList,j,j+1);
+                    j  -= await incrementAfterDelay();
+                }
+            }
+
+        }
+    }
+    async part(hList,barList,p,r){
+        let i = p-1;
+        for(let j = p; j < r; j++){
+            if( hList[j] < hList[r]){
+                i++;
+                this.swap(hList,i,j);
+                this.swapBars(barList,i,j);
+                i += await incrementAfterDelay();
+            }
+        }
+        this.swap(hList,i+1,r);
+        this.swapBars(barList,i+1,r);
+        i += await incrementAfterDelay();
+        return i+1;
+    }
+    async quickSortBars(hList,barList,p,r){
+        if ( p >= r) return;
+        let q = await this.part(hList,barList,p,r);
+        this.quickSortBars(hList,barList,p,q-1);
+       this.quickSortBars(hList,barList,q+1,r);
+    }
+    async insertSortBars(hList,barList){
+        for(let j = 1; j < hList.length;j++){
+            let key = hList[j];
+            //let bKey = barList[j].getCopy(); 
+            let i = j-1;
+            while( i >= 0 && hList[i] > key ){
+                hList[i+1] =hList[i];
+                this.swapBars(barList,i+1,i);
+                i  += await incrementAfterDelay(); // adds 0 
+                i--;
+            }
+            hList[i+1] = key;
+        }
+    }
+
+}
+
+
+//wait some time beofre continuing  // return 0 ;
+function incrementAfterDelay() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(0);
+      },100);
+    });
+}
